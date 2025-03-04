92ef464e3b44f429f805044e0df3399f7c64ab2d
Aydeny53 <Aydeny53@gmail.com>
2025-03-04 17:41:01 +0000

    Tue, Mar 4, 2025, 9:41 AM -08:00

--- js/event.js
+++ js/event.js
@@ -6,7 +6,7 @@ element1.addEventListener('click', ()=> {
     element1.innerHTML = "I've been clicked! <br> Try doubling clicking me!"
 })
 
-element1.addEventListener('dblclick' ()=> {
+element1.addEventListener('dblclick', ()=> {
     element1.innerHTML = "I've been double clicked! <br> Try single clicking me!"
 })
 
