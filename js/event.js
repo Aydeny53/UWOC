dac056acbfa47614565cab244106ac946863736e
Aydeny53 <Aydeny53@gmail.com>
2025-03-04 18:15:27 +0000

    Tue, Mar 4, 2025, 10:15 AM -08:00

--- js/event.js
+++ js/event.js
@@ -65,4 +65,5 @@ const list = ['running', 'paused']
 let index = 0
 element5.addEventListener('click', ()=> {
     element5.style.animationPlayState = list[index]
+    index = (index + 1) % 2 /* Modulo 2*/
 })
