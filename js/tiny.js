d77410c1020e866bc2c92c6e80b028638ec01b98
Aydeny53 <Aydeny53@gmail.com>
2025-03-08 07:22:36 +0000

    Fri, Mar 7, 2025, 11:22 PM -08:00

--- js/tiny.js
+++ js/tiny.js
@@ -1,15 +1,20 @@
 const little = document.querySelector('.little')
 const food = document.querySelector('.food')
+const text = document.querySelector('.text')
 
 
 let positionY = 0;
-let positionX = 0; 
+let positionX = 0;
 
 // let posY = food.style.top
 // let possX = food.style.left
 //if positionX > posY tionY > posY
-let foodPosY = food.style.top + 775;
-let foodPosX = food.style.left + 800;
+// let foodPosY = food.style.top + 775;
+// let foodPosX = food.style.left + 800;
+
+let foodPosY = 60;
+let foodPosX = 75;
+
 document.addEventListener('keyup', (event)=> {
     console.log(event.key)
 
@@ -38,10 +43,11 @@ document.addEventListener('keyup', (event)=> {
     //     little.style.backgroundColor = "transparent";
     // }
 
-    if (little.style.bottom > foodPosY && little.style.right > foodPosX) {
+    // little.style.bottom
+    if (positionY > foodPosY && positionX > foodPosX) {
         little.style.backgroundColor = "blue";
     }
-    else if (little.style.bottom < foodPosY || little.style.right < foodPosX) {
+    else if (positionY < foodPosY || positionX < foodPosX) {
         little.style.backgroundColor = "transparent";
     }
 
@@ -49,10 +55,14 @@ document.addEventListener('keyup', (event)=> {
         positionY = positionY - 10
         positionX = positionX - 10
     }
+    if (positionY == 125 || positionX == 125) {
+        positionY = 55
+        positionX = 45
+        text.innerHTML = "Why did you do that? :( <br> He got lost"
+
+
+    }
 
     little.style.bottom = `${positionY}%`
     little.style.right = `${positionX}%`
-
-
-
 })
