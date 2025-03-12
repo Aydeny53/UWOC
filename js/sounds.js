53690a20bd20a035bc93b5d2b51f0317dd13d839
Aydeny53 <Aydeny53@gmail.com>
2025-03-12 09:10:26 -0700

    Sound

--- js/sounds.js
+++ js/sounds.js
@@ -1,17 +1,19 @@
 const sounds = ['applause', 'boo', 'tada', 'gasp', 'victory', 'wrong']
 
+
 sounds.forEach(sound => {
+
     const btn = document.createElement('button')
     btn.classList.add('btn')
     btn.innerHTML = sound
-    btn.addEventListener('click', => {
+    btn.addEventListener('click', ()=> {
         stopSongs()
         document.getElementById(sound).play()
-
     })
 
     document.getElementById('buttons').appendChild(btn)
 })
+
 
 
 function stopSongs(){
