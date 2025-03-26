0c265e050318c3990fa7b93d7819593bb6f3996f
Aydeny53 <Aydeny53@gmail.com>
2025-03-26 18:35:07 +0000

    Wed, Mar 26, 2025, 11:35 AM -07:00

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const menu = document.querySelector('.menu')
 menu.addEventListener('click', ()=> {
     menu.classList.toggle("active")
     setTimeout ( ()=> menu.classList.toggle("doubleActive"), 50)
-    setTimeout ( ()=> menu.classList.toggle("doubleActive"), 50)
+    setTimeout ( ()=> menu.classList.toggle("tripleActive"), 50)
 
     // menu.classList.add('active')
     // menu.classList.remove('active')
