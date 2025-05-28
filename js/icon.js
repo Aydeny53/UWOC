ac78f67d3dab2a32c54c349ec9a74d2207ba2d1c
Aydeny53 <Aydeny53@gmail.com>
2025-05-28 15:48:01 +0000

    Wed, May 28, 2025, 8:48 AM -07:00

--- js/icon.js
+++ js/icon.js
@@ -3,7 +3,22 @@ const btn = document.querySelector('.btn')
 
 
 let number = 1
+let total = 0
 let randNum = Math.random() * 50
+const table = [
+    {
+        image: 'images/full.png',
+        text: "I'm Thirsty"
+    },
+    {
+        image: 'images/keepit.jpg',
+        text: "I'm Thirsty"
+    },
+    {
+        image: 'images/milly.png',
+        text: "I'm Thirsty"
+    }
+]
 console.log(randNum)
 
 btn.addEventListener('click', ()=> {
