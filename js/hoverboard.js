9dbfd11b9c30a717816d3330a90f019e457cc20d
Aydeny53 <Aydeny53@gmail.com>
2025-05-28 16:19:43 +0000

    Wed, May 28, 2025, 9:19 AM -07:00

--- js/hoverboard.js
+++ js/hoverboard.js
@@ -9,4 +9,12 @@ for(let i = 0; i < SQUARES; i = i+1){
     container.appendChild(square)
 }
 
-function setColor
+function setColor(element){
+    const color = getRandomColor()
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function getRandomColor(){
+    return colors[]
+}
