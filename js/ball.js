88b4806ac2b286cbbf7e36f64d490365b051cf10
Aydeny53 <Aydeny53@gmail.com>
2025-06-04 18:11:19 +0000

    Wed, Jun 4, 2025, 11:11 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -374,7 +374,7 @@ function movePaddle(){
     LPaddle.style.top = `${LPaddleYPosition}px`
 }
 
-let randomNum = Math.floor(Math.random() * 9) + 1
+let randomNum = Math.floor(Math.random() * 4) + 6
 function moveRPaddle(){
 
     if (level2 < 5){
