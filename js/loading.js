b2538c9abdc9250d181f4dd56e563b5bf7a3b6bb
Aydeny53 <Aydeny53@gmail.com>
2025-12-11 17:45:53 -0800

    Buncha stuff

--- js/loading.js
+++ js/loading.js
@@ -3,19 +3,25 @@ const bg = document.querySelector('.bg')
 
 let load = 0
 
-let int = setInterval(blurring, 100)
-
+let int = setInterval(blurring, 150)
 
 function blurring() {
     load = load + 1
+    bg.style.filter = `blur(${80 - load/2}px)`
+    loadText.style.opacity = `${1 - load / 200}`
+    loadText.innerText = `${load}%`
     if (load > 99){
+        load = load + 3
+        loadText.innerText = "Complete"
+        wait(1)
+        // bg.style.filter = "blur(0px)"
+        loadText.style.opacity = `${1 - load / 100}`
         clearInterval(int)
+
     }
 
 
-    loadText.innerText = `${load}%`
-    bg.style.filter = `blur(${50 - load/2}px)`
-    loadText.style.opacity = `${1 - load / 100}`
+
 }
 
 
--- js/search.js
+++ js/search.js
@@ -1,8 +1,32 @@
 const search = document. querySelector('.search')
 const btn = document.querySelector('.btn')
 const input = document.querySelector('.input')
+const submitButton = document.querySelector('.submit')
 
 btn.addEventListener('click', () => {
     search.classList.toggle('active')
     input.focus()
 })
+
+submitButton.addEventListener("click", myFunction)
+
+
+
+function myFunction(){
+    let userInput = document.querySelector('#Tasha');
+    let message = document.querySelector('#Message');
+    if (userInput.value == "hi" || userInput.value == "Hi" || userInput.value == "hello" || userInput.value == "Hello") {
+            message.innerHTML = "HI THEREEEEEE ";
+    }
+    else if (userInput.value == "uwoc" || userInput.value == "UWoC" || userInput.value == "UWOC") {
+        message.innerHTML = "It's the best thing ever!";
+    }
+    else if (userInput.value == "") {
+        message.innerHTML = message.innerHTML;
+    }
+    else {
+        message.innerHTML = "Huh, doesn't seem like that's anything...";
+    }
+    return;
+
+}
